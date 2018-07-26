a26968c5e98fa1b6af707d520c79a3977dc478fe
jithin <k.jithin12@gmail.com>
2018-07-26 12:11:53 +0530

    contacts search implemented

--- MyContacts/Contacts/Views/ContactsViewController.swift
+++ MyContacts/Contacts/Views/ContactsViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class ContactsViewController: BaseViewController {
     
-    var contacts: [Contact] = []
     @IBOutlet weak var contactsTableView: UITableView!
+
+    var contacts: [Contact] = []
+    var filteredContacts: [Contact] = []
+    let searchController = UISearchController(searchResultsController: nil)
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
         addListeners()
     }
     
@@ -66,19 +74,45 @@ class ContactsViewController: BaseViewController {
         self.navigationController?.pushViewController(addContactVC, animated: true)
     }
     
+    fileprivate func searchBarIsEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    fileprivate func filterContactForSearchText( _ text: String) {
+        filteredContacts = contacts.filter({ (contact) -> Bool in
+            return contact.name.lowercased().contains(text.lowercased())
+        })
+        contactsTableView.reloadData()
+    }
+    
+    fileprivate func isSearchActive() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
+    
 }
 
 extension ContactsViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isSearchActive() {
+            return filteredContacts.count
+        }
         return contacts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifiers.contactListCell, for: indexPath) as! ContactListCell
-        cell.nameLabel.text = contacts[indexPath.row].name
-        cell.phoneLabel.text = contacts[indexPath.row].phone
-        cell.profileImageView.setImage(string: contacts[indexPath.row].name, color: .green)
+        
+        let contact: Contact
+        if isSearchActive(){
+            contact = filteredContacts[indexPath.row]
+        }
+        else{
+            contact = contacts[indexPath.row]
+        }
+        cell.nameLabel.text = contact.name
+        cell.phoneLabel.text = contact.phone
+        cell.profileImageView.setImage(string: contact.name, color: .green)
         
         return cell
     }
@@ -96,4 +130,17 @@ extension ContactsViewController : UITableViewDelegate {
         Logger.log("cell selected with contact \(contacts[indexPath.row])")
     }
 }
+
+extension ContactsViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        guard let text = searchController.searchBar.text else { return }
+        filterContactForSearchText(text)
+    }
+    
+    
+}
+
+
 
